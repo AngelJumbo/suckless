cf6b9cfdf60bda7c9f8c6cd7df506faf8afd10ae
AngelJumbo <anarjumb@gmail.com>
2022-11-16 12:45:41 -0500

    Update dwmblocks config

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,15 +3,34 @@
 // the ^c#*^ are fg colors form https://dwm.suckless.org/patches/status2d/ 
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"^b#d79921^^c#1d2021^  ^b#fabd2f^ ", "block-battery perc BAT0",  					60,	 0},
+    {"^b#458588^^c#1d2021^  ^b#83a598^ ", "block-temperature /sys/class/thermal/thermal_zone0/temp",	5,	 0},
+    {"^b#98971a^^c#1d2021^  ^b#b8bb26^ ", "block-cpu perc",    					2,	 0},
+    {"^b#689d6a^^c#1d2021^  ^b#8ec07c^ ", "block-ram used",    					10,	 0},
+    {"^b#b16286^^c#1d2021^  ^b#d3869b^ ", "block-datetime \"%H:%M \"",					60,	 0},
+
+};
+
+
+/*
+    {"^b#d79921^^c#1d2021^  ^d^^c#fabd2f^ ", "block-battery perc BAT0",  				60,	 0},
+    {"^b#458588^^c#1d2021^  ^d^^c#83a598^ ", "block-temperature /sys/class/thermal/thermal_zone0/temp", 5,	 0},
+    {"^b#98971a^^c#1d2021^  ^d^^c#b8bb26^ ", "block-cpu perc",    					2,	 0},
+    {"^b#689d6a^^c#1d2021^  ^d^^c#8ec07c^ ", "block-ram used",    					10,	 0},
+    {"^b#b16286^^c#1d2021^  ^d^^c#d3869b^ ", "block-datetime \"%H:%M \"",		60,	 0},
+*/
+
+/*
+
     {" ^c#d79921^ ", "block-battery perc BAT0",  				60,	 0},
     {"^c#83a598^ ", "block-temperature /sys/class/thermal/thermal_zone0/temp", 5,	 0},
     {"^c#b8bb26^ ", "block-cpu perc",    					2,	 0},
     {"^c#8ec07c^ ", "block-ram used",    					10,	 0},
     {"^c#d3869b^ ", "block-datetime \"%H:%M  %a. %d %b. %Y\"",		60,	 0},
 
-};
+*/
 
 // sets delimeter between status commands. NULL character ('\0') means no
 // delimeter.
-static char delim[] ="  ";
-static unsigned int delimLen = 2;
+static char delim[] =" ";
+static unsigned int delimLen = 1;
